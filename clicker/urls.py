50d6e3b15eabab8691874adfc975f998d3541727
MullayanovRustam <rustam.mullayanov@gmail.com>
2021-06-11 19:23:44 +0500

    создание шаблона для страниц. стилизация шапки сайта. создание страниц login и register

--- clicker/urls.py
+++ clicker/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login', login, name='login'),
+    path('register', register, name='register')
 ]
 
 
--- clicker/views.py
+++ clicker/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'clicker/base.html')
+    return render(request, 'clicker/home.html')
 
+
+def login(request):
+    return render(request, 'clicker/login.html')
+
+
+def register(request):
+    return render(request, 'clicker/register.html')
